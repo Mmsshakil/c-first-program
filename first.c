50712d4de06fd3216a8a8a71fa5497907ade1a51
Mmsshakil <softhandshakil@gmail.com>
2025-04-07 00:15:42 +0600

    input and output done

--- first.c
+++ first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int a = 10;
-    float f = 4.343;
-    char c = 's';
-    printf("%d, %0.2f, %c", a, f, c);
+    int num1;
+    float f;
+    char c;
+    scanf("%d %f %c", &num1, &f, &c);
+    printf("%d %0.3f %c", num1, f, c);
     return 0;
 }
